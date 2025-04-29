67d801969d79560dd7bad91231b7e79af21f6aba
Hollister Herhold <hherhold@amnh.org>
2025-04-29 10:49:12 -0400

    Fixes for Faherty apr 23 talk
     - Removed boxed labels
     - fix make.bat to work like makefile

--- make.bat
+++ make.bat
@@ -9,12 +9,36 @@ set REPO_PATH=%~dp0
 
 echo %REPO_PATH%
 
-echo Cleaning up OpenSpace cache and assets...
-python .\clean_openspace_cache.py -c E:\git\OpenSpace\cache\ -a E:\OpenSpace\user\data\assets\faherty_apr_23
+: Use a command line argument to determine what to build. If "faherty_apr_23" is passed,
+: it will build assets for Faherty Apr 23 talk.
 
-cd data/Jan_30_2025_recentered
-python %REPO_PATH%/group_dataset.py -i eukaryotes_classes.csv -c kingdom
-cd %REPO_PATH%
+: If "faherty_apr_23" is not passed, it will build assets for the Jan 30, 2025 talk.
+: If no argument is passed, it will build assets for the Jan 30, 2025 talk.
 
-cd data/Jan_30_2025_recentered
-python %REPO_PATH%/csv_to_openspace.py -i faherty_apr_23.json -a E:\OpenSpace\user\data\assets\faherty_apr_23 -o ./outfiles_faherty_apr_23 -t E:\git\cosmic-view-life\textures
+IF "%1"=="faherty_apr_23" (
+    echo Cleaning up OpenSpace cache and assets...
+    python .\clean_openspace_cache.py -c E:\git\OpenSpace\cache\ -a E:\OpenSpace\user\data\assets\faherty_apr_23
+
+    cd data/Jan_30_2025_recentered
+    python %REPO_PATH%/group_dataset.py -i eukaryotes_classes.csv -c kingdom
+    cd %REPO_PATH%
+
+    cd data/Jan_30_2025_recentered
+    python %REPO_PATH%/csv_to_openspace.py -i faherty_apr_23.json -a E:\OpenSpace\user\data\assets\faherty_apr_23 -o ./outfiles_faherty_apr_23 -t E:\git\cosmic-view-life\textures
+
+) ELSE IF "%1"=="jan_30_2025_recentered" (
+    echo Cleaning up OpenSpace cache and assets...
+    python .\clean_openspace_cache.py -c E:\git\OpenSpace\cache\ -a E:\OpenSpace\user\data\assets\Jan_30_2025_recentered
+
+    cd data/Jan_30_2025_recentered
+    python %REPO_PATH%/group_dataset.py -i eukaryotes_classes.csv -c kingdom
+    cd %REPO_PATH%
+
+    cd data/Jan_30_2025_recentered
+    python %REPO_PATH%/csv_to_openspace.py -i jan_30_2025_recentered.json -a E:\OpenSpace\user\data\assets\jan_30_2025_recentered -o ./outfiles_jan_30_2025_recentered -t E:\git\cosmic-view-life\textures
+
+) ELSE (
+    echo No valid argument passed. Please pass "faherty_apr_23" or "jan_30_2025_recentered".
+)
+
+
